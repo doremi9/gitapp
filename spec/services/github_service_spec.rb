12477b6e307d28bc1860484c3b9fe95699cddc52
doremi9 <morehousecarrington@gmail.com>
2017-11-13 23:45:08 +0100

    Removed unnecessary controller spec

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe GithubService do
 
-  let(:token)     { '123abc' }
+  let(:token) { '123abc' }
 
   specify '' do
 
     ENV['GITHUB_ACCESS_TOKEN'] = token
 
     service = described_class.new(user, token)
+    puts service
     # expect ...
     #
     expect { service.call }.to change{Organization.count}.by(1)
